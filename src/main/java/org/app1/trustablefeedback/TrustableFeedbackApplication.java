c176dbc3bbf5bfa0733cabefe90d9d7806cb6dbb
GustavoCostaAM <gustavo.costa.m949@gamil.com>
2025-07-18 16:09:08 -0300

    initial commit

---
+++ src/main/java/org/app1/trustablefeedback/TrustableFeedbackApplication.java
@@ -0,0 +1,13 @@
+package org.app1.trustablefeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustableFeedbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrustableFeedbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/app1/trustablefeedback/TrustableFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.app1.trustablefeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustableFeedbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
